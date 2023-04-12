7e9e5b1728bc32b7213ff75f5134c7989a1cffc5
Ayesha3910 <113667421+Ayesha3910@users.noreply.github.com>
2023-04-12 10:51:44 -0600

    Add files via upload

---
+++ MADLABT.js
@@ -0,0 +1,59 @@
+/**length: Returns the length of a string.
+
+ * let str = "Hello World";
+console.log(str.length); // Output: 11/** */
+
+
+/**toLowerCase: Converts all characters in a string to lowercase.
+
+ * let str = "Hello World";
+console.log(str.toLowerCase()); // Output: hello world/** */
+
+
+/**toUpperCase: Converts all characters in a string to uppercase.
+
+ * let str = "Hello World";
+console.log(str.toUpperCase()); // Output: HELLO WORLD/** */
+
+
+/**charAt: Returns the character at a specified index in a string.
+
+ * let str = "Hello World";
+console.log(str.charAt(4)); // Output: o/** */
+
+
+/**substring: Returns a portion of a string starting from a specified index and ending at a specified index.
+
+ * let str = "Hello World";
+console.log(str.substring(0, 5)); // Output: Hello/** */
+
+
+/**indexOf: Returns the index of the first occurrence of a specified substring in a string.
+
+ * let str = "Hello World";
+console.log(str.indexOf("o")); // Output: 4/** */
+
+
+/**replace: Replaces a specified substring in a string with another substring.
+
+ * let str = "Hello World";
+console.log(str.replace("World", "Universe")); // Output: Hello Universe/** */
+
+
+/**trim: Removes whitespace from both ends of a string.
+
+ * let str = "   Hello World   ";
+console.log(str.trim()); // Output: Hello World/** */
+
+
+/**concat: Joins two or more strings together.
+
+ * let str1 = "Hello";
+let str2 = "World";
+console.log(str1.concat(" ", str2)); // Output: Hello World/** */
+
+
+/**split: Splits a string into an array of substrings based on a specified separator.
+
+ * let str = "Hello World";
+console.log(str.split(" ")); // Output: ["Hello", "World"]/** */
